bfadb6593ce65c0cbfc26d1b8d8799099b607dc3
valentin-mathieu <91801774+valentin-mathieu@users.noreply.github.com>
2022-03-22 12:13:50 +0100

    jour06 jusqu'à job04

---
+++ jour06/job01/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="style1.css">
+  <link rel="stylesheet" href="style2.css">
+  <link rel="stylesheet" href="style3.css">
+  <link rel="stylesheet" href="style4.css">
+</head>
+<body>
+</body>
+</html>
